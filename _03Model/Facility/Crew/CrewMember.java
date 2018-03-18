dac1ed90eea692b3e20dce81240b008fb9a319fa
Moises Puyosa <mpuyosa91@gmail.com>
2018-03-18 14:53:10 -0500

    Creacion de prototipo de cuenta Este prototipo tiene como finalidad el poder crear usuarios con distintos accesos a la plataforma, como atencion, modificacion de inventario, y auditoria.

--- _03Model/Facility/Crew/CrewMember.java
+++ _03Model/Facility/Crew/CrewMember.java
@@ -10,5 +10,58 @@ package _03Model.Facility.Crew;
  * @author mpuyosa91
  */
 public class CrewMember {
+    public enum Permision{
+        Attendant(1),
+        Manager(2),
+        Administrator(4);
+        
+        private final int permissionCode;
+        Permision(int permission){
+            this.permissionCode = permission;
+        }
+        public int getPermisionCode(){
+            return this.permissionCode;
+        }
+    }
     
+    public CrewMember(){
+        
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String nombre) {
+        this.user = nombre;
+    }
+    
+    public String getHash(){
+        return hash;
+    }
+    
+    public void setHash(String hash){
+        this.hash = hash;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public short getPermisionLevel() {
+        return permisionLevel;
+    }
+
+    public void setPermisionLevel(short permisionLevel) {
+        this.permisionLevel = permisionLevel;
+    }
+    
+    private String user;
+    private String hash;
+    private int id;
+    private short permisionLevel;
 }
